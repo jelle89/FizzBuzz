4ad3cf85e7ff76cbcd3598d6d1410a24edfed2f4
jelle89 <jelle.monen@hotmail.com>
2019-11-11 17:08:09 +0100

    added php

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,12 @@
-for (let i = 1; i < 101; i++) {
+for (let i = 1; i < 102; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
   } else if (i % 5 === 0) {
     console.log("Buzz");
   } else if (i % 3 === 0) {
     console.log("Fizz");
+  } else if (i > 100) {
+    console.log("After this point you should stop drinking!");
   } else {
     console.log(i);
   }
---
+++ FizzBuzz.php
@@ -0,0 +1,15 @@
+<?php
+for ($i = 1; $i < 102; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
+        echo 'FizzBuzz<br>';
+    } elseif ($i % 3 === 0) {
+        echo 'Fizz<br>';
+    } elseif ($i % 5 === 0) {
+        echo 'Buzz<br>';
+    } elseif ($i > 100) {
+        echo 'After this point you should stop drinking!<br>';
+    } else {
+        echo $i;
+        echo "<br>";
+    }
+}
